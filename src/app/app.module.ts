09e911a3b6509c042cc395646144d3e497e14571
Somsubhra Das <das.somsubhra1@gmail.com>
2020-07-01 14:15:15 +0530

    Todos template created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { TodosComponent } from './components/todos/todos.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 import { HeaderComponent } from './layout/header/header.component';
 
+// FontAwesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
     TodoFormComponent,
-    HeaderComponent
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
