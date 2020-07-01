6a65f8e6716efa39d864aadee1428f41ef467f79
Somsubhra Das <das.somsubhra1@gmail.com>
2020-07-01 13:55:15 +0530

    Todo component ts file done

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  faTrashAlt = faTrashAlt;
 
-  constructor() { }
+  todos: Todo[];
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.todoService.getTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
   }
 
+  changeTodoStatus(todo: Todo) {
+    this.todoService.changeStatus(todo);
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo);
+  }
 }
