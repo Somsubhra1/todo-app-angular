2fecbbab7e6991bedb7e7441ccfa57270cbcd260
Somsubhra Das <das.somsubhra1@gmail.com>
2020-07-01 13:30:37 +0530

    Todo service done

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { Todo } from '../models/Todo';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  todos: Todo[];
 
-  constructor() { }
+  constructor() {
+    this.todos = [
+      {
+        id: '111',
+        title: 'Learn C++',
+        isCompleted: true,
+        date: new Date(),
+      },
+      {
+        id: '222',
+        title: 'Learn React',
+        isCompleted: true,
+        date: new Date(),
+      },
+      {
+        id: '333',
+        title: 'Learn Angular',
+        isCompleted: false,
+        date: new Date(),
+      },
+    ];
+  }
+
+  getTodos() {
+    /*of is used to fetch the values after the values are assigned to the todo variable by the constructor. So we need to observe it*/
+    return of(this.todos);
+  }
+
+  addTodo(todo: Todo) {
+    this.todos.push(todo);
+  }
+
+  changeStatus(todo: Todo) {
+    this.todos.map((singleTodo) => {
+      if (singleTodo.id === todo.id) {
+        todo.isCompleted = !todo.isCompleted;
+      }
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    const indexOfTodo = this.todos.findIndex(
+      (currentObject) => currentObject.id === todo.id
+    );
+    this.todos.splice(indexOfTodo, 1);
+  }
 }
